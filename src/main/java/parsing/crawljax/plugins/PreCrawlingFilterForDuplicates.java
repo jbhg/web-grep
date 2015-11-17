8284432bbff19ff9d0089360c38b19626997965c
Joel B. Greenberg <joelgreenberg26@gmail.com>
2015-11-17 16:04:52 -0500

    A rough next iteration. Not pretty, though.

--- src/main/java/parsing/crawljax/plugins/PreCrawlingFilterForDuplicates.java
+++ src/main/java/parsing/crawljax/plugins/PreCrawlingFilterForDuplicates.java
@@ -1,11 +1,14 @@
 package parsing.crawljax.plugins;
 
-import app.Debug;
 import com.crawljax.core.CandidateElement;
 import com.crawljax.core.CrawlerContext;
 import com.crawljax.core.plugin.PreStateCrawlingPlugin;
 import com.crawljax.core.state.StateVertex;
 import com.google.common.collect.ImmutableList;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 /**
  * This class is intended to look at the available objects to determine
@@ -13,13 +16,37 @@ import com.google.common.collect.ImmutableList;
  */
 public class PreCrawlingFilterForDuplicates implements PreStateCrawlingPlugin
 {
+    private static Set<String> _seenSites;
+
+    public PreCrawlingFilterForDuplicates()
+    {
+        _seenSites = new HashSet<>();
+    }
     @Override
     public void preStateCrawling(CrawlerContext context, ImmutableList<CandidateElement> candidateElements, StateVertex state)
     {
-        Debug.println("Joel was here.");
-        for (CandidateElement element : candidateElements.asList())
+        Set<CandidateElement> elementsToRemove = new HashSet<>();
+        System.out.println("Current page: " + context.getCurrentState().getUrl());
+        for(CandidateElement i : state.getCandidateElements())
         {
-            System.out.println("Element: " + element.getElement().getTagName());
+            if (i.getElement().hasAttribute("href"))
+            {
+                System.out.println("\tcandidate has link: " + i.getElement().getAttribute("href"));
+                if (!_seenSites.add(i.getElement().getAttribute("href")))
+                {
+                    System.out.println("\t\tRemoving link: " + i.getElement().getAttribute("href"));
+                    elementsToRemove.add(i);
+                }
+            }
         }
+        LinkedList<CandidateElement> newElements = new LinkedList<>();
+        for (CandidateElement i : state.getCandidateElements())
+        {
+            if (!elementsToRemove.contains(i))
+            {
+                newElements.add(i);
+            }
+        }
+        state.setElementsFound(newElements);
     }
 }
